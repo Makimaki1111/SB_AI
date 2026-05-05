d65644f83b06f5bb59e068abc743474213e3b956
Makimaki1111 <118779192+Makimaki1111@users.noreply.github.com>
2026-05-05 15:38:32 +0900

    入力欄サイズ

--- frontend-v2/src/components/battle/BattleArena.tsx
+++ frontend-v2/src/components/battle/BattleArena.tsx
@@ -122,15 +122,14 @@ export const BattleArena: React.FC<BattleArenaProps> = ({
               </div>
             )}
           </div>
-        </div>
 
-        <div className={styles.actionsWrapper}>
+          <div className={styles.actionsWrapper}>
             <div 
               className={`${styles.actionBtn} ${styles.situationBtn}`} 
               onClick={onOpenSituation}
             >
               <svg className={styles.actionBtnIcon} viewBox="0 0 24 24">
-                <path d="M13,9H11V7H13M13,17H11V11H13M12,2A10,10,0,0,0,2,12A10,10,0,0,0,12,22A10,10,0,0,0,22,12A10,10,0,0,0,12,2Z" />
+                <path d="M3 13.125C3 12.5037 3.50368 12 4.125 12H6.75C7.37132 12 7.875 12.5037 7.875 13.125V18.375C7.875 18.9963 7.37132 19.5 6.75 19.5H4.125C3.50368 19.5 3 18.9963 3 18.375V13.125ZM10.125 7.125C10.125 6.50368 10.6287 6 11.25 6H13.875C14.4963 6 15 6.50368 15 7.125V18.375C15 18.9963 14.4963 19.5 13.875 19.5H11.25C10.6287 19.5 10.125 18.9963 10.125 18.375V7.125ZM17.25 3.375C17.25 2.75368 17.7537 2.25 18.375 2.25H21C21.6213 2.25 22.125 2.75368 22.125 3.375V18.375C22.125 18.9963 21.6213 19.5 21 19.5H18.375C17.7537 19.5 17.25 18.9963 17.25 18.375V3.375Z" />
               </svg>
               <span className={styles.actionBtnText}>状況</span>
             </div>
@@ -139,10 +138,11 @@ export const BattleArena: React.FC<BattleArenaProps> = ({
               onClick={onOpenAbility}
             >
               <svg className={styles.actionBtnIcon} viewBox="0 0 24 24">
-                <path d="M12,2L4.5,20.29L5.21,21L12,18L18.79,21L19.5,20.29L12,2Z" />
+                <path d="M3.75 13.5l10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75z" />
               </svg>
               <span className={styles.actionBtnText}>特性</span>
             </div>
+          </div>
         </div>
 
         <div className={styles.footerArea}>
